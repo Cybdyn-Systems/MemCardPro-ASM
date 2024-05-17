2fbae34feb8964e3f8cc67b0390e06aae42fd7a9
Jonathan Cel <61208676+JonathanDotCel@users.noreply.github.com>
2024-05-17 16:46:01 +0100

    Add a basic C header to the obj files

---
+++ mcpro.h
@@ -0,0 +1,29 @@
+// Apache V2 License
+
+
+#ifndef MCPRO_H
+#define MCPRO_H
+
+//
+// Asm functions
+//
+
+extern int MemCardPro_Ping(int port01);
+extern int MemCardPro_SendGameID(int port01, int maxLen255, char *gameID);
+
+//
+// Errors:
+//
+
+#define MCPRO_0_NO_ERROR
+#define MCPRO_1_BUS_SELECT_FAIL
+// e.g no ping response, failed to change channel, etc.
+#define MCPRO_2_OPERATION_FAILED
+#define MCPRO_3_RESERVE_FAIL
+#define MCPRO_4_STRING_LENGTH_FAIL
+#define MCPRO_5_DATA_TRANSMISSION_FAIL
+
+#define MCPRO_PORT_0 0
+#define MCPRO_PORT_1 1
+
+#endif /* MCPRO_H */
